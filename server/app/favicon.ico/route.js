480f55616260c49b5b29b8b181a182182c1ebfad
chandan <chandankl0310@gmail.com>
2023-08-08 21:38:10 +0530

    Updates

---
+++ server/app/favicon.ico/route.js
@@ -0,0 +1,113 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 155;
+exports.ids = [155];
+exports.modules = {
+
+/***/ 4021:
+/***/ ((module) => {
+
+module.exports = import("next/dist/compiled/@vercel/og/index.node.js");;
+
+/***/ }),
+
+/***/ 2037:
+/***/ ((module) => {
+
+module.exports = require("os");
+
+/***/ }),
+
+/***/ 8651:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  headerHooks: () => (/* binding */ headerHooks),
+  originalPathname: () => (/* binding */ originalPathname),
+  requestAsyncStorage: () => (/* binding */ requestAsyncStorage),
+  routeModule: () => (/* binding */ routeModule),
+  serverHooks: () => (/* binding */ serverHooks),
+  staticGenerationAsyncStorage: () => (/* binding */ staticGenerationAsyncStorage),
+  staticGenerationBailout: () => (/* binding */ staticGenerationBailout)
+});
+
+// NAMESPACE OBJECT: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Ffavicon.ico%2Froute&isDynamic=0!./src/app/favicon.ico?__next_metadata_route__
+var favicon_next_metadata_route_namespaceObject = {};
+__webpack_require__.r(favicon_next_metadata_route_namespaceObject);
+__webpack_require__.d(favicon_next_metadata_route_namespaceObject, {
+  GET: () => (GET),
+  dynamic: () => (dynamic)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/node-polyfill-headers.js
+var node_polyfill_headers = __webpack_require__(2394);
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/app-route/module.js
+var app_route_module = __webpack_require__(9692);
+var module_default = /*#__PURE__*/__webpack_require__.n(app_route_module);
+// EXTERNAL MODULE: ./node_modules/next/server.js
+var server = __webpack_require__(514);
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Ffavicon.ico%2Froute&isDynamic=0!./src/app/favicon.ico?__next_metadata_route__
+
+
+const contentType = "image/x-icon"
+const buffer = Buffer.from("AAABAAEAICAAAAEAIACoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwAAAFkAAACbAAAAzAAAAOwAAAD8AAAA/AAAAOwAAADMAAAAmwAAAFkAAAAPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJgAAAJ0AAAD2AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAPYAAACdAAAAJgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACQAAAIwAAAD6AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD6AAAAjAAAAAkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACUAAADUAAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA1AAAACUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAyAAAA6wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA6wAAADIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJQAAAOsAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA6wAAACUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAkAAADUAAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA1AAAAAkAAAAAAAAAAAAAAAAAAAAAAAAAjAAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAAjAAAAAAAAAAAAAAAAAAAACYAAAD6AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD6AAAAJgAAAAAAAAAAAAAAnQAAAP8AAAD/AAAA/wAAAP8AAAD/AQEB/0JCQv99fX3/iIiI/2tra/8gICD/AAAA/wAAAP8AAAD/AAAA/wkJCf9fX1//Kioq/wAAAP8AAAD/AAAA/wAAAP8AAAD/QUFB/19fX/8RERH/AAAA/wAAAP8AAACdAAAAAAAAAA8AAAD2AAAA/wAAAP8AAAD/AAAA/yEhIf/Kysr///////X19f/o6Oj//f39//v7+/+JiYn/AgIC/wAAAP8AAAD/FxcX//////9xcXH/AAAA/wAAAP8AAAD/AAAA/zY2Nv/5+fn/p6en/wAAAP8AAAD/AAAA/wAAAPYAAAAPAAAAWQAAAP8AAAD/AAAA/wAAAP8SEhL/4ODg/+jo6P9TU1P/BAQE/wAAAP8VFRX/jY2N//////+Hh4f/AAAA/wAAAP8XFxf//////3Fxcf8AAAD/AAAA/wAAAP8RERH/39/f/9fX1/8MDAz/AAAA/wAAAP8AAAD/AAAA/wAAAFkAAACbAAAA/wAAAP8AAAD/AAAA/5OTk//4+Pj/LS0t/wAAAP8AAAD/AAAA/wAAAP8AAAD/lZWV//r6+v8bGxv/AAAA/xcXF///////cXFx/wAAAP8AAAD/AAAA/66urv/09PT/Kysr/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAAmwAAAMwAAAD/AAAA/wAAAP8GBgb/8vLy/5mZmf8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8gICD/mZmZ/zY2Nv8AAAD/FxcX//////94eHj/AAAA/wAAAP9vb2///////1xcXP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAADMAAAA7AAAAP8AAAD/AAAA/zQ0NP//////T09P/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8XFxf//////+/v7/8sLCz/NTU1//n5+f+Xl5f/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAOwAAAD8AAAA/wAAAP8AAAD/TU1N//////8wMDD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/xcXF////////////+Pj4//e3t7/y8vL/wcHB/8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/AAAAPwAAAD/AAAA/wAAAP9NTU3//////zExMf8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/FxcX//////+Ojo7/4eHh//////8wMDD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD8AAAA7AAAAP8AAAD/AAAA/zMzM///////UFBQ/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8XFxf//////3Fxcf8oKCj/7Ozs/8PDw/8KCgr/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAOwAAADMAAAA/wAAAP8AAAD/BgYG//Hx8f+cnJz/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/ISEh/5+fn/83Nzf/AAAA/xcXF///////cXFx/wAAAP84ODj/9PT0/7a2tv8GBgb/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAAzAAAAJsAAAD/AAAA/wAAAP8AAAD/kJCQ//n5+f8wMDD/AAAA/wAAAP8AAAD/AAAA/wAAAP+ZmZn/+fn5/xoaGv8AAAD/FxcX//////9xcXH/AAAA/wAAAP9JSUn/+vr6/6ioqP8CAgL/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAACbAAAAWQAAAP8AAAD/AAAA/wAAAP8QEBD/3d3d/+vr6/9aWlr/BwcH/wAAAP8aGhr/lJSU//////+CgoL/AAAA/wAAAP8XFxf//////3Fxcf8AAAD/AAAA/wAAAP9eXl7//v7+/5mZmf8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAFkAAAAPAAAA9gAAAP8AAAD/AAAA/wAAAP8eHh7/xMTE///////5+fn/7u7u//7+/v/6+vr/g4OD/wEBAf8AAAD/AAAA/xcXF///////cXFx/wAAAP8AAAD/AAAA/wAAAP91dXX//////4iIiP8AAAD/AAAA/wAAAP8AAAD2AAAADwAAAAAAAACdAAAA/wAAAP8AAAD/AAAA/wAAAP8BAQH/PDw8/3d3d/+Dg4P/ZWVl/xsbG/8AAAD/AAAA/wAAAP8AAAD/CAgI/1lZWf8oKCj/AAAA/wAAAP8AAAD/AAAA/wAAAP9MTEz/WVlZ/w8PD/8AAAD/AAAA/wAAAJ0AAAAAAAAAAAAAACYAAAD6AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD6AAAAJgAAAAAAAAAAAAAAAAAAAIwAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAIwAAAAAAAAAAAAAAAAAAAAAAAAACQAAANQAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAADUAAAACQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJQAAAOsAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA6wAAACUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMgAAAOsAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAOsAAAAyAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJQAAANQAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAADUAAAAJQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACQAAAIwAAAD6AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD6AAAAjAAAAAkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACYAAACdAAAA9gAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD2AAAAnQAAACYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPAAAAWQAAAJsAAADMAAAA7AAAAPwAAAD8AAAA7AAAAMwAAACbAAAAWQAAAA8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/8AD//8AAP/8AAA/+AAAH/AAAA/gAAAHwAAAA8AAAAOAAAABgAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAGAAAABwAAAA8AAAAPgAAAH8AAAD/gAAB/8AAA//wAA///AA/8=", 'base64'
+  )
+
+function GET() {
+  return new server.NextResponse(buffer, {
+    headers: {
+      'Content-Type': contentType,
+      'Cache-Control': "public, max-age=0, must-revalidate",
+    },
+  })
+}
+
+const dynamic = 'force-static'
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-app-loader.js?page=%2Ffavicon.ico%2Froute&name=app%2Ffavicon.ico%2Froute&pagePath=private-next-app-dir%2Ffavicon.ico&appDir=%2Fhome%2Fchandan%2FPractice%2Fportfolio%2Fsrc%2Fapp&appPaths=%2Ffavicon.ico&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D!
+
+    
+
+    
+
+    
+
+    const options = {"definition":{"kind":"APP_ROUTE","page":"/favicon.ico/route","pathname":"/favicon.ico","filename":"favicon","bundlePath":"app/favicon.ico/route"},"resolvedPagePath":"next-metadata-route-loader?page=%2Ffavicon.ico%2Froute&isDynamic=0!/home/chandan/Practice/portfolio/src/app/favicon.ico?__next_metadata_route__","nextConfigOutput":""}
+    const routeModule = new (module_default())({
+      ...options,
+      userland: favicon_next_metadata_route_namespaceObject,
+    })
+
+    // Pull out the exports that we need to expose from the module. This should
+    // be eliminated when we've moved the other routes to the new format. These
+    // are used to hook into the route.
+    const {
+      requestAsyncStorage,
+      staticGenerationAsyncStorage,
+      serverHooks,
+      headerHooks,
+      staticGenerationBailout
+    } = routeModule
+
+    const originalPathname = "/favicon.ico/route"
+
+    
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [697,218], () => (__webpack_exec__(8651)));
+module.exports = __webpack_exports__;
+
+})();
