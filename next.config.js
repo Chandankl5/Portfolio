223f65b9857b0f39d2420f10b4eae73d05fbe45a
chandan <chandankl0310@gmail.com>
2023-08-09 23:53:00 +0530

    Update next config for GH pages deployment

--- next.config.js
+++ next.config.js
@@ -10,4 +10,14 @@ const nextConfig = {
   }
 }
 
+if(process.env.EXPORT) {
+  nextConfig.output = 'export';
+}
+
+if(process.env.GH_PAGES_DEPLOY) {
+  nextConfig.assetPrefix = '/Portfolio';
+  nextConfig.images.loader = 'custom';
+  nextConfig.images.loader = './src/utils/imageLoader.js';
+}
+
 module.exports = nextConfig
